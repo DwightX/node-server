2b43ead71c20164fb727244c5fb8bb659457dc0d
Dwight Carter <dwighthowichill@icloud.com>
2021-04-13 01:09:48 -0400

    listen to port 80

--- server.js
+++ server.js
@@ -51,5 +51,6 @@ app.get("/insults", function(req, res) {
 
 app.use("/public", express.static("./public"));
 
-app.listen(3000);
-console.log("listening on http://localhost:3000");
+const port = process.env.PORT || 3000;
+app.listen(port);
+console.log(`listening on http://localhost:${port}`);
